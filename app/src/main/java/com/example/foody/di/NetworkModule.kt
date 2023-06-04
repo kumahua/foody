ab4480611633360eb1632035f94bb0391344e05d
hua <a4704704@gmail.com>
2023-06-04 14:37:29 +0800

    add loggingInterceptor

--- app/src/main/java/com/example/foody/di/NetworkModule.kt
+++ app/src/main/java/com/example/foody/di/NetworkModule.kt
@@ -2,11 +2,13 @@ package com.example.foody.di
 
 import com.example.foody.util.Constants.Companion.BASE_URL
 import com.example.foody.data.network.FoodRecipesApi
+import com.orhanobut.logger.Logger
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -19,7 +21,11 @@ object NetworkModule {
     @Singleton
     @Provides
     fun provideHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
         return OkHttpClient.Builder()
+            .addNetworkInterceptor(loggingInterceptor)
             .readTimeout(15, TimeUnit.SECONDS)
             .connectTimeout(15, TimeUnit.SECONDS)
             .build()
